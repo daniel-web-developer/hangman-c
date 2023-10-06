0bbcd3487792797292d330d3764527f977e38ee9
Daniel <daniel_caixeta@protonmail.com>
2023-10-06 12:31:10 +0200

    almost done

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ char *getWord() {
 
 }
 
-char guessChar() {
+char guessChar(const char *guessedLetters) {
 
     char guess;
 
@@ -54,10 +54,36 @@ char guessChar() {
 
     if (!isalpha(guess)) {
         printf("\nPlease type a letter: ");
-        guessChar();
+        return guessChar(guessedLetters);
+    }
+
+    for (int i = 0; i < strlen(guessedLetters); i++) {
+        if (guessedLetters[i] == guess) {
+            printf("You've already tried this one. Please type a different letter: ");
+            return guessChar(guessedLetters);
+        }
     }
 
     return guess;
+}
+
+void drawGui(const char *answer, const char *guessed, int index, int chances) {
+
+    int drawDummy = 5 - chances;
+
+    printf("\n\n");
+    printf("Chances left: %d\n", chances);
+    printf("Word: %s ", answer);
+
+    if (index > 0) {
+        printf("Letters guessed: ");
+        for (int i = 0; i < index; i++) {
+            printf("%c", guessed[i]);
+        }
+    }
+
+    printf("\n\n");
+
 }
 
 int game() {
@@ -65,22 +91,47 @@ int game() {
     char *word = getWord();
     int length = strlen(word);
     char guessedLetters[length + 6];
+    int indexGuessedLetters = 0;
     char guess;
-    char guessed[length];
+    char answer[length];
+    int right = 0;
 
     int chances = 5;
 
     for (int i = 0; i < length; i++) {
-        guessed[i] = '_';
+        answer[i] = '_';
     }
+
+    if (word[1] == '-') {
+        answer[1] = '-';
+    }
+
+    printf("%s\n", word);
 
     while (chances > 0) {
 
+        drawGui(answer, guessedLetters, indexGuessedLetters, chances);
+
         printf("Guess a letter: ");
 
-        guess = guessChar();
+        guess = guessChar(guessedLetters);
+        guessedLetters[indexGuessedLetters] = guess;
+        indexGuessedLetters++;
 
-        chances--;
+        for (int j = 0; j < length; j++) {
+            if (guess == word[j]) {
+                answer[j] = guess;
+                right = 1; // indicate user got it right
+            } else if (j == length - 1 && right == 0) { // check if for loop ended and guess is right
+                chances--;
+            }
+        }
+
+        for (int k = 0; k < length; k++) {
+            printf("%c", answer[k]);
+        }
+
+        right = 0;
     }
 
     free(word);
