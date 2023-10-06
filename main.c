94e07d8228fc1cb782fc143685e52671df8734cb
Daniel <daniel_caixeta@protonmail.com>
2023-10-06 02:32:37 +0200

    inputting guess is done

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 int menu();
 
@@ -41,33 +42,71 @@ char *getWord() {
 
 }
 
+char guessChar() {
+
+    char guess;
+
+    guess = tolower(getchar());
+
+    int c;
+
+    while ((c = getchar()) != '\n' && c != EOF);
+
+    if (!isalpha(guess)) {
+        printf("\nPlease type a letter: ");
+        guessChar();
+    }
+
+    return guess;
+}
+
 int game() {
 
     char *word = getWord();
+    int length = strlen(word);
+    char guessedLetters[length + 6];
+    char guess;
+    char guessed[length];
 
-    printf("The word is %s\n", word);
+    int chances = 5;
+
+    for (int i = 0; i < length; i++) {
+        guessed[i] = '_';
+    }
+
+    while (chances > 0) {
+
+        printf("Guess a letter: ");
+
+        guess = guessChar();
+
+        chances--;
+    }
+
+    free(word);
 
     menu();
-
 }
 
 int menu() {
 
     int exit = 0;
-    int option;
+    char option;
 
     printf("\nChoose an option:\n");
     printf("1. Start new game.\n");
     printf("2. Leave.\n");
 
-    scanf("%d", &option);
+    scanf("%c", &option);
+
+    getchar(); // to consume the newline character
 
     switch(option) {
-        case 1:
+        case '1':
             game();
             break;
 
-        case 2:
+        case '2':
             printf("Goodbye!\n");
             return 0;
 
